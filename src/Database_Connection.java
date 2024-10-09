2553108d834208b2eed537f3c45465fde2793b58
Samridhi Gupta <samridhiguptaa02@gmail.com>
2024-10-09 18:33:43 +0530

    Update Database_Connection.java

--- src/Database_Connection.java
+++ src/Database_Connection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class Database_Connection {
         private static final String URL = "jdbc:mysql://localhost:3306/Student";
         private static final String USER = "root";
-        private static final String PASSWORD = "kripaluji";
+        private static final String PASSWORD = "root";
 
         public static Connection getConnection () {
             Connection connection = null;
